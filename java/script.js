ec83cf50bdd5232dff032d22ab34e6819598f5c2
Federico <fluna1973@yahoo.com>
2022-07-18 23:12:10 -0600

    complete mobile menu

---
+++ java/script.js
@@ -0,0 +1,50 @@
+
+
+const hamburger = document.querySelector('#hamburger');
+const modalMenu = document.querySelector('#modalMenu');
+
+hamburger.addEventListener('click', () => {
+  modalMenu.style.display = "block";
+})
+
+const button =  document.querySelector('.active');
+
+button.addEventListener('click', () => {
+  modalMenu.style.display = "none";
+})
+
+const lika = document.querySelector('#closebtntest');
+
+lika.addEventListener('click', () => {
+  modalMenu.style.display = "none";
+})
+
+const arrow = document.querySelector('#closebtntest2');
+
+arrow.addEventListener('click', () => {
+  modalMenu.style.display = "none";
+})
+
+const lika1 = document.querySelector('#closebtntest3');
+
+lika1.addEventListener('click', () => {
+  modalMenu.style.display = "none";
+})
+
+const arrow1 = document.querySelector('#closebtntest4');
+
+arrow1.addEventListener('click', () => {
+  modalMenu.style.display = "none";
+})
+
+const newlika = document.querySelector('#closebtnt');
+
+newlika.addEventListener('click', () => {
+  modalMenu.style.display = "none";
+})
+
+const newarrow = document.querySelector('#closebtn');
+
+newarrow.addEventListener('click', () => {
+  modalMenu.style.display = "none";
+})
